d31ffa765983861f87004de902072c5b76f17052
kirinzloh <weiquan7@hotmail.com>
2017-04-11 10:52:22 +0800

    Certificate verification done

--- ProgAssignment2/src/Authentication.java
+++ ProgAssignment2/src/Authentication.java
@@ -1,3 +1,5 @@
+package ProgAssignment2.src;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -8,32 +10,20 @@ import java.security.cert.X509Certificate;
 
 
 public class Authentication {
-
-    public static X509Certificate CAcert;
-    public static PublicKey key;
-
-
-    public Authentication() throws FileNotFoundException, CertificateException {
-        // create X509Certificate object
-        InputStream fis = new FileInputStream("C:\\Pinardy\\Term_5\\50.005 - Computer Systems Engineering\\ProgAssignment2\\1001520.crt");
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        CAcert =(X509Certificate)cf.generateCertificate(fis);
-
-        // extract public key from X509Certificate object
-        key = CAcert.getPublicKey();
-    }
-
-
-    public static void verify(X509Certificate signedCert) throws NoSuchProviderException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        signedCert.checkValidity();
-        signedCert.verify(key);
-    }
-
     public static void main(String[] args) throws FileNotFoundException, CertificateException {
         Authentication au = new Authentication();
-
+        
+        // create X509Certificate object for CA.crt
+        X509Certificate CAcert = au.CreateX509Cert("CA.crt");
+        
+        // create X509Certificate object for 1001520.crt
+        X509Certificate MyCert = au.CreateX509Cert("1001520.crt");
+        
+        // extract public key from CAcert
+        PublicKey key = CAcert.getPublicKey();
+        
         try {
-            au.verify(CAcert);
+            au.Verify(MyCert,key);
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
         } catch (CertificateException e) {
@@ -45,6 +35,24 @@ public class Authentication {
         } catch (SignatureException e) {
             e.printStackTrace();
         }
+    }
+    
+    public X509Certificate CreateX509Cert(String cert) throws CertificateException, FileNotFoundException{
+    	InputStream fis = new FileInputStream(cert);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate X509cert =(X509Certificate)cf.generateCertificate(fis);
+        return X509cert;
+    }
+
+    public void Verify(X509Certificate signedCert, PublicKey key) throws NoSuchProviderException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        try {
+        	signedCert.checkValidity();
+            signedCert.verify(key);
+            System.out.println("Certificate verified!");
+        } catch (Exception e){
+        	System.out.println("Certificate verification failed!");
+        }
+    
     }
 
 
